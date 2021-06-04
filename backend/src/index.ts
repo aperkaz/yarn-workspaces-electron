3e2eec7390f10ee7c949a64d436b5a05a725ee90
alain <alainperkaz@gmail.com>
2021-06-04 18:11:30 +0200

    Add TS support

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,5 @@
-const init = async () => {
-  const sharp = require("sharp");
+import { add, sharp } from "shared";
 
-  const imagePath = `/Users/alain/src/taggr/frontend/src/statics/background.jpg`;
-  const outputPath = `/Users/alain/Downloads/sharp-test.jpg`;
+sharp();
 
-  await sharp(imagePath, {
-    failOnError: false,
-  }) // failOnError: true, fixes Samsung corrupted pictures
-    .jpeg({ quality: 80 })
-    .resize(200, 200, { fit: sharp.fit.outside, withoutEnlargement: true })
-    .toFile(outputPath);
-};
-
-init();
-
-export default init;
+console.log("SUM: ", add(1, 2));
---
+++ shared/src/index.ts
@@ -0,0 +1,18 @@
+export const sharp = async () => {
+  const sharp = require("sharp");
+
+  const imagePath = `/Users/alain/src/taggr/frontend/src/statics/background.jpg`;
+  const outputPath = `/Users/alain/Downloads/sharp-test.jpg`;
+
+  await sharp(imagePath, {
+    failOnError: false,
+  }) // failOnError: true, fixes Samsung corrupted pictures
+    .jpeg({ quality: 80 })
+    .resize(200, 200, { fit: sharp.fit.outside, withoutEnlargement: true })
+    .toFile(outputPath);
+};
+
+/**
+ * Hi from module
+ */
+export const add = (a: number, b) => a + b;
