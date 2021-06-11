ac5b7223be15b34035eb166c43a3705ad07b1a48
alain <alainperkaz@gmail.com>
2021-06-11 14:05:56 +0200

    Update gitignore

--- shared/build/index.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class User {
-    name: string;
-    age: number;
-    constructor(name: string, age: number);
-    greet(): string;
-}
-export * as API from './API';
-//# sourceMappingURL=index.d.ts.map
